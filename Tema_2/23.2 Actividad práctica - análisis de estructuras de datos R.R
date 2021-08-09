24356632e22d071dde4c00d8b48d5fd1743ba0ca
Juan Perdomo <41835233+juancamilo1020@users.noreply.github.com>
2021-08-08 22:28:17 -0400

    actualización

---
+++ Tema_2/23.2 Actividad práctica - análisis de estructuras de datos R.R
@@ -0,0 +1,23 @@
+# Actividad práctica: análisis de estructuras de datos
+## Para esta actividad, debes borrar las rayas que están antes de los paréntesis 
+## y rellenar los espacios que quedan vacíos con la palabra o el texto faltantes 
+## en la línea de código, de manera que puedas hallar la respuesta. 
+
+# Punto 1. Se define la variable x igual a 5
+x <- _
+# Se halla el tipo o clase de variable de x
+class(_)
+
+# Punto 2. Se define la variable mot (motivación) como "R está una nota"
+mot <- "_ está _ nota"
+class(_)
+
+# Punto 3. Se define la variable p igual a 10 y la variable h igual a 5
+# Se hace una operación entre p + h, y se define como la variable w
+_ <- 10
+h <- _
+w <- _ + _
+# Se hace una operación para saber si w es menor que h, y se le le llama f
+f <- _ < _
+# Luego se quiere saber qué tipo de variable es f
+class(_)
